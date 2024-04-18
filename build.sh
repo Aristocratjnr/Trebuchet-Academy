d74706d0136224943b2a981d8623c729527cc2b0
Obuobi Ayim David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-04-18 16:51:25 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ pip install -r requirements.txt
 # Specify Python interpreter version explicitly if necessary
 python3.10 manage.py collectstatic --noinput
 
-# Run collectstatic command
-python manage.py collectstatic --noinput

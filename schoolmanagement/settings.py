fb9446cde313caafc9d21558bf039ff7a5fed662
Obuobi Ayim David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-04-18 17:56:18 +0000

    changes on settings

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -123,12 +123,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+
 
 
 # Login Redirect URL

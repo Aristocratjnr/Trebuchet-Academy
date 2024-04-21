eec728aec34cc0e34c2daadf8a3332a36a094cde
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-04-21 18:13:41 +0000

    rollback on the python version for vercel.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 pip install -r requirements.txt
 
 # Specify Python interpreter version explicitly if necessary
-python3.10 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 

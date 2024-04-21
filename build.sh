1f572d86894a0dd128b8a80cfd8c4ed5fbdd0138
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-04-21 17:43:33 +0000

    removal of py on build shell

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 pip install -r requirements.txt
 
 # Specify Python interpreter version explicitly if necessary
-python3.12 manage.py collectstatic --noinput
+python3.10 manage.py collectstatic --noinput
 
